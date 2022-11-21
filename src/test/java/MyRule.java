622c852fa908633451efddc89457e898861aa92b
RenzO102 <stson102@mail.ru>
2022-11-21 15:39:48 +0500

    1.5

--- src/test/java/MyRule.java
+++ src/test/java/MyRule.java
@@ -1,42 +1,39 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.atlas.core.Atlas;
-import io.qameta.atlas.core.api.Retry;
 import io.qameta.atlas.webdriver.WebDriverConfiguration;
 import io.qameta.atlas.webdriver.WebPage;
 import org.junit.rules.ExternalResource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.github.com.RenzO102.Page.MainPage;
 
-public  class MyRule extends ExternalResource {
-
-    public Atlas atlas;
-
-    public WebDriver driver;
+public class MyRule extends ExternalResource {
 
     public MainPage onMainPage() {
         return onPage(MainPage.class);
     }
 
-    public  <T extends WebPage> T onPage(Class<T> page) {
+    public Atlas atlas;
+    public WebDriver driver;
+
+    public <T extends WebPage> T onPage(Class<T> page) {
         return atlas.create(driver, page);
     }
 
     @Override
-    public void before() throws Throwable{
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            atlas = new Atlas(new WebDriverConfiguration(driver));
-            onMainPage().open("https://vk.com/feed");
-            onMainPage().searchLogin().sendKeys("stson102@ya.ru");
-            onMainPage().buttonNext().click();
-            onMainPage().searchPassword().sendKeys("Omon9876");
-            onMainPage().buttonEnt().click();
-        }
+    protected void before() throws Throwable {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        atlas = new Atlas(new WebDriverConfiguration(driver));
+        onMainPage().open("https://vk.com/feed");
+        onMainPage().searchLogin().sendKeys("stson102@ya.ru");
+        onMainPage().buttonNext().click();
+        onMainPage().searchPassword().sendKeys("Omon9876");
+        onMainPage().buttonEnt().click();
+    }
 
     @Override
     public void after() {
-            this.driver.quit();
-        }
+        this.driver.quit();
+    }
 }
--- src/test/java/TestAtlasVk.java
+++ src/test/java/TestAtlasVk.java
@@ -1,64 +1,49 @@
-import io.qameta.atlas.core.Atlas;
-import io.qameta.atlas.webdriver.WebPage;
-import org.junit.*;
-import org.openqa.selenium.WebDriver;
-import ru.github.com.RenzO102.Page.MainPage;
+import org.junit.ClassRule;
+import org.junit.Test;
 
-public  class TestAtlasVk  {
-
-    public Atlas atlas;
-
-    public WebDriver driver;
+public class TestAtlasVk extends MyRule {
 
     @ClassRule
     public static MyRule wdriver = new MyRule();
 
     @Test
     public void shouldFoundMusic() throws Exception {
-        onMainPage().leftMenu("Музыка").click();
-        onMainPage().playMusic().click();
+        wdriver.onMainPage().leftMenu("Музыка").click();
+        wdriver.onMainPage().playMusic().click();
     }
 
     @Test
-    public void shouldFoundGroup() {
-        onMainPage().leftMenu("Сообщества").click();
-        onMainPage().searchCom().sendKeys("Рифмы и Панчи");
+    public void shouldFoundGroup() throws Exception {
+        wdriver.onMainPage().leftMenu("Сообщества").click();
+        wdriver.onMainPage().searchCom().sendKeys("Рифмы и Панчи");
     }
 
     @Test
-    public void shouldSendLoveMessageToMyPassion() {
-        onMainPage().leftMenu("Друзья").click();
-        onMainPage().foundMessage().click();
-        onMainPage().writeMessage().sendKeys("Вот ты и дома");
-        onMainPage().sendMessage().click();
+    public void shouldSendLoveMessageToMyPassion() throws Exception {
+        wdriver.onMainPage().leftMenu("Друзья").click();
+        wdriver.onMainPage().foundMessage().click();
+        wdriver.onMainPage().writeMessage().sendKeys("Вот ты и дома");
+        wdriver.onMainPage().sendMessage().click();
     }
 
     @Test
-    public void shouldAddNewFreind() {
-        onMainPage().leftMenu("Друзья").click();
-        onMainPage().foundFriend().sendKeys("Юрий Калинин");
-        onMainPage().foundPerson().click();
-        onMainPage().addFreind().click();
+    public void shouldAddNewFreind() throws Exception {
+        wdriver.onMainPage().leftMenu("Друзья").click();
+        wdriver.onMainPage().foundFriend().sendKeys("Юрий Калинин");
+        wdriver.onMainPage().foundPerson().click();
+        wdriver.onMainPage().addFreind().click();
     }
 
     @Test
-    public void ShouldLikeNewMyPhoto() {
-        onMainPage().leftMenu("Фотографии").click();
-        onMainPage().photo().click();
-        onMainPage().like().click();
+    public void ShouldLikeNewMyPhoto() throws Exception {
+        wdriver.onMainPage().leftMenu("Фотографии").click();
+        wdriver.onMainPage().photo().click();
+        wdriver.onMainPage().like().click();
     }
 
     @Test
-    public void shouldAcceptMyPhoto() {
-        onMainPage().leftMenu("Фотографии").click();
-        onMainPage().accept().click();
-    }
-
-    public MainPage onMainPage() {
-        return onPage(MainPage.class);
-    }
-
-    public <T extends WebPage> T onPage(Class<T> page) {
-        return atlas.create(driver, page);
+    public void shouldAcceptMyPhoto() throws Exception {
+        wdriver.onMainPage().leftMenu("Фотографии").click();
+        wdriver.onMainPage().accept().click();
     }
 }
